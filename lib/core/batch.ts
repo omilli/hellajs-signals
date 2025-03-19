3cdcc64545c091c36cdd09f53729f4af9aae70bd
omilli <oli.millington@gmail.com>
2025-03-19 10:41:13 +0700

    organise and document

--- lib/core/batch.ts
+++ lib/core/batch.ts
@@ -1,31 +1,38 @@
 import { getCurrentContext } from "../context";
 import { flushEffects } from "../utils";
+
 /**
- * Batch multiple signal updates together
- * Effects will only run once at the end of the batch
+ * Batches a series of operations together, preventing intermediate effects from being flushed until the batch is complete.
+ *
+ * This function is used to group multiple state updates or other operations into a single batch,
+ * which can improve performance by reducing the number of times the UI needs to be updated or other side effects need to be executed.
+ *
+ * @param fn A function that contains the operations to be batched.
+ * @returns The result of the function.
+ *
  */
 export function batch<T>(fn: () => T): T {
-	const ctx = getCurrentContext();
+  const ctx = getCurrentContext();
 
-	// Increment batch depth
-	ctx.batchDepth = (ctx.batchDepth || 0) + 1;
+  // Increment batch depth
+  ctx.batchDepth = (ctx.batchDepth || 0) + 1;
 
-	try {
-		return fn();
-	} finally {
-		// Decrement batch depth
-		ctx.batchDepth = ctx.batchDepth - 1;
+  try {
+    return fn();
+  } finally {
+    // Decrement batch depth
+    ctx.batchDepth = ctx.batchDepth - 1;
 
-		// If we're back at the top level, flush any pending effects
-		if (ctx.batchDepth === 0) {
-			if (
-				typeof window !== "undefined" &&
-				typeof queueMicrotask === "function"
-			) {
-				queueMicrotask(() => flushEffects(ctx));
-			} else {
-				flushEffects(ctx);
-			}
-		}
-	}
+    // If we're back at the top level, flush any pending effects
+    if (ctx.batchDepth === 0) {
+      if (
+        typeof window !== "undefined" &&
+        typeof queueMicrotask === "function"
+      ) {
+        queueMicrotask(() => flushEffects(ctx));
+      } else {
+        flushEffects(ctx);
+      }
+    }
+  }
 }
